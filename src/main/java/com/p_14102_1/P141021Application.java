a6bac43fa5160be1cb28aa70e0747d2c3c73ac2e
장인호 <jih95349238@gmail.com>
2025-11-28 10:40:04 +0900

    work

---
+++ src/main/java/com/p_14102_1/P141021Application.java
@@ -0,0 +1,13 @@
+package com.p_14102_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P141021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P141021Application.class, args);
+	}
+
+}
